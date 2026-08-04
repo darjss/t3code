9907cfc01a05424814383fe74fc15272cfac5167
Theo Browne <me@t3.gg>
2026-08-04 16:33:09 -0700

    ci: comment thread transfer impact on pull requests

--- apps/server/src/server.test.ts
+++ apps/server/src/server.test.ts
@@ -172,6 +172,7 @@ import {
 } from "../integration/TransferBudgetScenario.integration.ts";
 import {
   formatTransferBudgetReport,
+  formatTransferBudgetResult,
   type TransferBudgetRun,
   transferBudgetViolations,
 } from "../integration/TransferBudgetReport.integration.ts";
@@ -7922,6 +7923,13 @@ it.live(
       if (Option.isSome(reportPath)) {
         const fileSystem = yield* FileSystem.FileSystem;
         yield* fileSystem.writeFileString(reportPath.value, report);
+      }
+      const resultPath = yield* Config.string("T3CODE_TRANSFER_BUDGET_RESULT_PATH").pipe(
+        Config.option,
+      );
+      if (Option.isSome(resultPath)) {
+        const fileSystem = yield* FileSystem.FileSystem;
+        yield* fileSystem.writeFileString(resultPath.value, formatTransferBudgetResult(runs));
       }
       assert.deepEqual(transferBudgetViolations(runs), []);
     }).pipe(Effect.provide(NodeServices.layer)),
